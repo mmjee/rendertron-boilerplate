f995164ce1ce9a5c752b207a45476a0b71ee8b49
root <root@git.maharshi.ninja>
2021-11-02 04:41:10 +0530

    Add matrix and trust proxy

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const rendertron = require('rendertron-middleware')
 const app = express()
 
 app.use(morgan('combined'))
+app.set('trust proxy', 1)
 
 const STATIC = path.resolve(__dirname, '/app/frontend')
 const INDEX = path.resolve(STATIC, 'index.html')
 const port = process.env.HTTP_PORT ? Number(process.env.HTTP_PORT) : 80
 
+// List of UAs I have discovered to be bots that can't handle SPAs
 rendertron.botUserAgents.push('google')
 rendertron.botUserAgents.push('discord')
 rendertron.botUserAgents.push('slurp')
 rendertron.botUserAgents.push('DuckDuckBot')
+rendertron.botUserAgents.push('matrix')
 
 app.use(rendertron.makeMiddleware({
   proxyUrl: process.env.RENDERTRON_URL
