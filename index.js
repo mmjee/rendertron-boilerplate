5e3ea6fb9f6ad24e94f6b415037e2781d5ee8702
root <root@git.maharshi.ninja>
2021-11-02 05:44:35 +0530

    Reimplement using Pupperender

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
-const rendertron = require('rendertron-middleware')
+const pupperender = require('pupperender')
 
 const app = express()
 
@@ -13,15 +13,13 @@ const INDEX = path.resolve(STATIC, 'index.html')
 const port = process.env.HTTP_PORT ? Number(process.env.HTTP_PORT) : 80
 
 // List of UAs I have discovered to be bots that can't handle SPAs
-rendertron.botUserAgents.push('google')
-rendertron.botUserAgents.push('discord')
-rendertron.botUserAgents.push('slurp')
-rendertron.botUserAgents.push('DuckDuckBot')
-rendertron.botUserAgents.push('matrix')
+pupperender.botUserAgents.push('google')
+pupperender.botUserAgents.push('discord')
+pupperender.botUserAgents.push('slurp')
+pupperender.botUserAgents.push('DuckDuckBot')
+pupperender.botUserAgents.push('matrix')
 
-app.use(rendertron.makeMiddleware({
-  proxyUrl: process.env.RENDERTRON_URL
-}))
+app.use(pupperender.makeMiddleware({}))
 app.use(express.static(STATIC))
 app.get('*', function (req, res) {
   res.sendFile(INDEX)
