5a77ef6184d7271348f7bf08b9e45740c787ad01
root <root@git.maharshi.ninja>
2022-02-24 07:24:32 +0530

    Reorder execution

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const STATIC = path.resolve(__dirname, '/app/frontend')
 const INDEX = path.resolve(STATIC, 'index.html')
 const port = process.env.HTTP_PORT ? Number(process.env.HTTP_PORT) : 80
 
+app.use(express.static(STATIC))
 app.use(rendertron.makeMiddleware({
   proxyUrl: process.env.RENDERTRON_URL
 }))
-app.use(express.static(STATIC))
 app.get('*', function (req, res) {
   res.sendFile(INDEX)
 })
